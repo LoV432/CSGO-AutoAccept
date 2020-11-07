b59669896c19ff5140b046949151a2d778cef0dd
LoV432 <60856741+LoV432@users.noreply.github.com>
2020-11-08 01:34:55 +0500

    Update accept.py

--- accept.py
+++ accept.py
@@ -1,35 +1,48 @@
 from time import sleep
-from pyautogui import pixelMatchesColor, click, moveTo, getActiveWindow
+from pyautogui import pixelMatchesColor, click, moveTo, getActiveWindow, press
 from screeninfo import get_monitors
+import keyboard
 
-while True:
+def accept(keyboard_event):
+    
+    if keyboard.is_pressed("1") is True:
+        print("Script Active ", end="\r")
+        sleep(0.2)
+        while True:
+            # Add Delay
 
-    # Add Delay
-    sleep(5)
+            # Get active window
+            # (Used try-except becuz it throws error when no window focused)
+            try:
+                window = getActiveWindow().title
+            except:
+                window = "NULL"
 
-    # Get active window
-    # (Used try-except becuz it throws error when no window focused)
-    try:
-        window = getActiveWindow().title
-    except:
-        window = "NULL"
+            # If active window = cs
+            if window == "Counter-Strike: Global Offensive":
 
-    # If active window = cs
-    if window == "Counter-Strike: Global Offensive":
+                # Get current res and position of accept button
+                x1 = round(get_monitors()[0].width / 100 * 50)
+                y1 = round(get_monitors()[0].height / 100 * 56.64)
 
-        # Get current res and position of accept button
-        x1 = round(get_monitors()[0].width / 100 * 50)
-        y1 = round(get_monitors()[0].height / 100 * 56.64)
+                # Take SS and compare the pixel x1,y1 with the given rgb value
+                check1 = pixelMatchesColor(x1, y1, (78, 175, 82), tolerance=5)
+                check2 = pixelMatchesColor(x1, y1, (93, 203, 98), tolerance=5)
 
-        # Take SS and compare the pixel x1,y1 with the given rgb value
-        check1 = pixelMatchesColor(x1, y1, (78, 175, 82), tolerance=5)
-        check2 = pixelMatchesColor(x1, y1, (93, 203, 98), tolerance=5)
+                # If match found go to x1,y1 and click
+                if check1 or check2 == True:
+                    
+                    # Click accept
+                    click(x1, y1)
 
-        # If match found go to x1,y1 and click
-        if check1 or check2 == True:
-            
-            # Click accept
-            click(x1, y1)
+                    # Move mouse out of the way (Not really needed but ¯\_(ツ)_/¯)
+                    moveTo(100, 200)
 
-            # Move mouse out of the way (Not really needed but ¯\_(ツ)_/¯)
-            moveTo(100, 200)
+                if keyboard.is_pressed("1") is True:
+                    print("Script Paused", end="\r")
+                    sleep(0.2)
+                    break
+
+print('Press TAB to activate or pause\n')
+keyboard.hook_key("1", accept)
+keyboard.wait()
